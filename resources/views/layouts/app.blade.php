f683cb85d660c5c7f7f81e2cb24d67730c301fb4
Kay1l <142229866+kay1l@users.noreply.github.com>
2025-07-11 15:28:12 +0800

    feat: image preview when clicked

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,97 +1,202 @@
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <title>Aside - Free HTML5 Bootstrap 4 Template by uicookies.com</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Free HTML5 Website Template by uicookies.com">
-    <meta name="keywords"
-        content="free bootstrap 4, free bootstrap 4 template, free website templates, free html5, free template, free website template, html5, css3, mobile first, responsive">
-    <meta name="author" content="uicookies.com">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <head>
+        <title>Aside - Free HTML5 Bootstrap 4 Template by uicookies.com</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Free HTML5 Website Template by uicookies.com">
+        <meta name="keywords"
+            content="free bootstrap 4, free bootstrap 4 template, free website templates, free html5, free template, free website template, html5, css3, mobile first, responsive">
+        <meta name="author" content="uicookies.com">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/open-iconic-bootstrap.min.css') }}">
+        <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/open-iconic-bootstrap.min.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/icomoon.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{asset('assets/css/loading.css')}}">
-    <meta name="robots" content="noindex, nofollow">
-</head>
+        <link rel="stylesheet" href="{{ asset('assets/css/icomoon.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/loading.css') }}">
+        <meta name="robots" content="noindex, nofollow">
+    </head>
 
-<body>
+    <body>
 
-    @include('partials.sidebar')
+        @include('partials.sidebar')
 
-    <main role="main" class="probootstrap-main js-probootstrap-main">
+        <main role="main" class="probootstrap-main js-probootstrap-main">
 
-        <div id="photo-loader" style="display: none; text-align: center; padding: 20px;">
-            <i class="fa fa-spinner fa-spin" style="font-size: 24px; color: #333;"></i>
-            <p>Loading new photos...</p>
-        </div>
+            <div id="photo-loader" style="display: none; text-align: center; padding: 20px;">
+                <i class="fa fa-spinner fa-spin" style="font-size: 24px; color: #333;"></i>
+                <p>Loading new photos...</p>
+            </div>
 
-        {{-- <button id="manual-refresh-btn" class="refresh-button" aria-label="Refresh">
-            <i class="fa fa-refresh"></i>
-          </button> --}}
+            {{-- <button id="manual-refresh-btn" class="refresh-button" aria-label="Refresh">
+                <i class="fa fa-refresh"></i>
+            </button> --}}
 
-        <div class="probootstrap-bar">
-            <a href="#" class="probootstrap-toggle js-probootstrap-toggle"><span class="fa fa-bars"></span></a>
-            <div class="probootstrap-main-site-logo"><a href="index.html">MuseumCam</a></div>
-        </div>
-        <div class="card-columns">
-            @forelse ($captures as $capture)
-                <div class="card img-loaded">
-                    <a href="{{ asset('storage/photos/' . $capture->filename) }}" target="_blank">
-                        <img class="card-img-top probootstrap-animate fadeInUp"
-                             src="{{ asset('storage/photos/' . $capture->filename) }}"
-                             alt="Captured photo">
+            <div class="probootstrap-bar">
+                <a href="#" class="probootstrap-toggle js-probootstrap-toggle"><span
+                        class="fa fa-bars"></span></a>
+                <div class="probootstrap-main-site-logo"><a href="index.html">MuseumCam</a></div>
+            </div>
+            <div class="card-columns">
+                <div class="card img-loaded image-preview-link" data-src="images/img_1.jpg">
+                    <a href="#">
+                        <img class="card-img-top probootstrap-animate fadeInUp probootstrap-animated"
+                             src="images/img_1.jpg" alt="Card image cap">
                     </a>
                 </div>
-            @empty
-                <p>No photos found for this album.</p>
-            @endforelse
-        </div>
-
-        <div class="container-fluid d-md-none">
-            <div class="row">
-                <div class="col-md-12">
-                    <ul class="list-unstyled d-flex probootstrap-aside-social">
-                        <li><a href="#" class="p-2"><span class="fa fa-twitter"></span></a></li>
-                        <li><a href="#" class="p-2"><span class="fa fa-instagram"></span></a></li>
-                        <li><a href="#" class="p-2"><span class="fa fa-linkedin"></span></a></li>
-                    </ul>
-                    <p>© 2017 <a href="https://uicookies.com/" target="_blank">uiCookies:Aside</a>. <br> All Rights
-                        Reserved. Designed by <a href="https://uicookies.com/" target="_blank">uicookies.com</a></p>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate fadeInUp probootstrap-animated"
+                            src="images/img_2.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate fadeInUp probootstrap-animated"
+                            src="images/img_3.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_4.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_5.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_6.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_7.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_8.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate fadeInUp probootstrap-animated"
+                            src="images/img_9.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate fadeInUp probootstrap-animated"
+                            src="images/img_10.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate fadeInUp probootstrap-animated"
+                            src="images/img_11.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_12.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_13.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_14.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_15.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate fadeInUp probootstrap-animated"
+                            src="images/img_16.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate fadeInUp probootstrap-animated"
+                            src="images/img_17.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_18.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_19.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_20.jpg" alt="Card image cap">
+                    </a>
+                </div>
+                <div class="card img-loaded">
+                    <a href="#" class="image-preview-link">
+                        <img class="card-img-top probootstrap-animate" src="images/img_21.jpg" alt="Card image cap">
+                    </a>
                 </div>
             </div>
-        </div>
 
-    </main>
-    @include('partials.shared-modal')
+            <div class="container-fluid d-md-none">
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul class="list-unstyled d-flex probootstrap-aside-social">
+                            <li><a href="#" class="p-2"><span class="fa fa-twitter"></span></a></li>
+                            <li><a href="#" class="p-2"><span class="fa fa-instagram"></span></a></li>
+                            <li><a href="#" class="p-2"><span class="fa fa-linkedin"></span></a></li>
+                        </ul>
+                        <p>© 2017 <a href="https://uicookies.com/" target="_blank">uiCookies:Aside</a>. <br> All
+                            Rights
+                            Reserved. Designed by <a href="https://uicookies.com/" target="_blank">uicookies.com</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
 
-    <script src="{{ asset('assets/js/jquery-3.2.1.slim.min.js') }}"></script>
-    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
-    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.waypoints.min.js') }}"></script>
-    <script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
-    <script src="{{ asset('assets/js/photo.js') }}"></script>
-    <script src="{{ asset('assets/js/refresh.js') }}"></script>
+        @include('partials.shared-modal')
 
-    <script src="{{ asset('assets/js/main.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery-3.2.1.slim.min.js') }}"></script>
+        <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.waypoints.min.js') }}"></script>
+        <script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
+        <script src="{{ asset('assets/js/photo.js') }}"></script>
+        <script src="{{ asset('assets/js/refresh.js') }}"></script>
 
-    <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015"
-        integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ=="
-        data-cf-beacon='{"rayId":"95cc2a000e3efd22","version":"2025.6.2","r":1,"token":"1aea4905526a46c1a4fc9bc1d0e89a42","serverTiming":{"name":{"cfExtPri":true,"cfEdge":true,"cfOrigin":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}}}'
-        crossorigin="anonymous"></script>
+        <script src="{{ asset('assets/js/main.js') }}"></script>
 
-</body>
+        <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015"
+            integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ=="
+            data-cf-beacon='{"rayId":"95cc2a000e3efd22","version":"2025.6.2","r":1,"token":"1aea4905526a46c1a4fc9bc1d0e89a42","serverTiming":{"name":{"cfExtPri":true,"cfEdge":true,"cfOrigin":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}}}'
+            crossorigin="anonymous"></script>
 
-</html>
+    </body>
+
+    </html>
--- resources/views/partials/shared-modal.blade.php
+++ resources/views/partials/shared-modal.blade.php
@@ -29,14 +29,43 @@
         font-size: 24px;
         cursor: pointer;
     }
+
+    #image-preview-container img {
+        max-width: 100%;
+        height: auto;
+        border-radius: 10px;
+    }
+
+    #image-preview-text {
+        color: white;
+        margin-top: 1rem;
+    }
+
+    #image-preview-modal .modal-content {
+        background: transparent;
+        padding: 0;
+        max-width: 90%;
+    }
+
+    #image-preview-modal .close-button {
+        color: white;
+        top: 5px;
+        right: 10px;
+    }
 </style>
 
 <div id="sidebar-modal" class="modal" style="display: none;">
     <div class="modal-content">
         <span class="close-button" id="close-sidebar-modal">&times;</span>
-        <div id="modal-body">
-            <!-- Modal content will be injected here -->
-        </div>
+        <div id="modal-body"></div>
+    </div>
+</div>
+
+<div id="image-preview-modal" class="modal">
+    <div class="modal-content" id="image-preview-container">
+        <span class="close-button" id="close-image-preview">&times;</span>
+        <img id="preview-image" src="" alt="Preview" />
+        <p id="image-preview-text">Long press the image and tap "Save to Photos"</p>
     </div>
 </div>
 
@@ -45,10 +74,8 @@
         const modal = document.getElementById('sidebar-modal');
         const modalBody = document.getElementById('modal-body');
         const closeBtn = document.getElementById('close-sidebar-modal');
+        const accessType = '{{ $accessType ?? 'live' }}';
 
-        const accessType = '{{ $accessType ?? 'live' }}'; // Blade/templating fallback
-
-        // Filter based on data-mode
         document.querySelectorAll('[data-mode]').forEach(item => {
             if (item.getAttribute('data-mode') !== accessType) {
                 item.style.display = 'none';
@@ -65,22 +92,19 @@
             modalBody.innerHTML = '';
         });
 
-        // Event bindings
         document.getElementById('receive-pictures')?.addEventListener('click', e => {
             e.preventDefault();
             openModal(`
-      <h3><i class="fa fa-envelope mr-2"></i> Receive all my pictures</h3>
-      <p>Enter your email to receive your album link after your visit.</p>
-      <input type="email" id="email-input" placeholder="you@example.com" style="width:100%; padding:10px;" />
-      <button id="submit-email" style="margin-top:10px;">Submit</button>
-    `);
-
+                <h3><i class="fa fa-envelope mr-2"></i> Receive all my pictures</h3>
+                <p>Enter your email to receive your album link after your visit.</p>
+                <input type="email" id="email-input" placeholder="you@example.com" style="width:100%; padding:10px;" />
+                <button id="submit-email" style="margin-top:10px;">Submit</button>
+            `);
             setTimeout(() => {
                 document.getElementById('submit-email').addEventListener('click', () => {
                     const email = document.getElementById('email-input').value.trim();
                     if (email) {
-                        alert(
-                            `You’ll receive your album link at ${email} after your visit.`);
+                        alert(`You’ll receive your album link at ${email} after your visit.`);
                         modal.style.display = 'none';
                     } else {
                         alert("Please enter a valid email.");
@@ -91,33 +115,25 @@
 
         document.getElementById('invite-friend-qr')?.addEventListener('click', async e => {
             e.preventDefault();
-
             const albumId = {{ $album->id ?? 'null' }};
-
             if (!albumId) return alert('Missing album ID');
-
             try {
                 const res = await fetch('/photographer/invite-qr', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
-                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')
-                            .getAttribute('content')
+                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
                     },
-                    body: JSON.stringify({
-                        album_id: albumId
-                    })
+                    body: JSON.stringify({ album_id: albumId })
                 });
-
                 const data = await res.json();
-
                 if (data.success) {
                     openModal(`
-        <h3><i class="fa fa-mobile mr-2"></i> Invite a friend to this album</h3>
-        <p>Scan this QR code to open the same live album on another phone.</p>
-        <img src="${data.qr_svg}" alt="QR Code" style="max-width:200px; margin: 1rem 0;" />
-        <p>This QR code works only while this album is alive. A new QR code replaces the one printed on the device.</p>
-      `);
+                        <h3><i class="fa fa-mobile mr-2"></i> Invite a friend to this album</h3>
+                        <p>Scan this QR code to open the same live album on another phone.</p>
+                        <img src="${data.qr_svg}" alt="QR Code" style="max-width:200px; margin: 1rem 0;" />
+                        <p>This QR code works only while this album is alive. A new QR code replaces the one printed on the device.</p>
+                    `);
                 } else {
                     alert('QR generation failed.');
                 }
@@ -127,16 +143,14 @@
             }
         });
 
-
         document.getElementById('invite-friend-email')?.addEventListener('click', e => {
             e.preventDefault();
             openModal(`
-      <h3><i class="fa fa-user-plus mr-2"></i> Invite a friend by Email</h3>
-      <p>Enter your friend’s email to invite them to access this album.</p>
-      <input type="email" id="friend-email" placeholder="friend@example.com" style="width:100%; padding:10px;" />
-      <button id="submit-invite" style="margin-top:10px;">Send Invitation</button>
-    `);
-
+                <h3><i class="fa fa-user-plus mr-2"></i> Invite a friend by Email</h3>
+                <p>Enter your friend’s email to invite them to access this album.</p>
+                <input type="email" id="friend-email" placeholder="friend@example.com" style="width:100%; padding:10px;" />
+                <button id="submit-invite" style="margin-top:10px;">Send Invitation</button>
+            `);
             setTimeout(() => {
                 document.getElementById('submit-invite').addEventListener('click', () => {
                     const email = document.getElementById('friend-email').value.trim();
@@ -148,6 +162,30 @@
                     }
                 });
             }, 0);
+        });
+
+        const imgModal = document.getElementById('image-preview-modal');
+        const imgPreview = document.getElementById('preview-image');
+        const closeImgModal = document.getElementById('close-image-preview');
+
+        document.querySelectorAll('.image-preview-link').forEach(img => {
+            img.addEventListener('click', function (e) {
+                e.preventDefault();
+                imgPreview.src = this.dataset.fullSrc || this.src;
+                imgModal.style.display = 'flex';
+            });
+        });
+
+        closeImgModal.addEventListener('click', () => {
+            imgModal.style.display = 'none';
+            imgPreview.src = '';
+        });
+
+        imgModal.addEventListener('click', (e) => {
+            if (e.target === imgModal) {
+                imgModal.style.display = 'none';
+                imgPreview.src = '';
+            }
         });
     });
 </script>
