e733345dcbb3d251a21ded8728f1a2114def6e5c
Kay1l <142229866+kay1l@users.noreply.github.com>
2025-07-24 23:35:25 +0800

    new

--- start.sh
+++ start.sh
@@ -6,11 +6,13 @@ php artisan cache:clear
 php artisan route:clear
 php artisan view:clear
 
+echo "🔗 Linking storage folder..."
+php artisan storage:link || echo "Storage link already exists."
+
+
 echo "🔁 Running migrations and seeders..."
 php artisan migrate --seed --force
 
 echo "🚀 Starting Apache..."
 apache2-foreground
 
-echo "🔗 Linking storage folder..."
-php artisan storage:link || echo "Storage link already exists."
